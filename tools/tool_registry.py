80132c2e1e8d7e4f731e251d01d13699ceebbc8b
GabrielNixon <gr2513@nyu.edu>
2025-06-23 17:17:30 -0400

    registry

--- tools/tool_registry.py
+++ tools/tool_registry.py
@@ -1,8 +1,12 @@
 from system_summary_agent import summarize_from_json
 
+def filter_args(func, args_dict):
+    allowed = func.__code__.co_varnames[:func.__code__.co_argcount]
+    return {k: v for k, v in args_dict.items() if k in allowed}
+
 TOOL_REGISTRY = {
     "slack_summary": {
-        "function": summarize_from_json,
-        "args": ["start_date", "end_date", "target"]
+        "function": lambda **kwargs: summarize_from_json(**filter_args(summarize_from_json, kwargs)),
+        "args": ["start_date", "end_date"]
     }
 }
